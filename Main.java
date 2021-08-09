5d390461a7516f0f06bea29b95be8b2accfe62b3
Motasim <makkimotasim@gmail.com>
2021-08-09 20:34:43 +0530

    feat: get host name and address

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
-import java.util.Scanner;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 class Main{
     public static void main(String[] args) {
-        
+        String host_name =  "";
+        try{
+            InetAddress ip = InetAddress.getByName("MOTASIM-PC");
+            System.out.println("System Name: " + ip.getHostName());
+            System.out.println("System IP-Address: " + ip.getHostAddress());
+        }catch(UnknownHostException exc){
+            System.out.println("Not found the IP-ADDRESS for: " + exc);
+        }
     }
 }
